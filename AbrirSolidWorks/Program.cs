ec206bec9a542fcfd7d4657c6129cbe2a25a3a45
GetulioSSFilho <getuliosergio@hotmail.com>
2022-10-17 19:37:45 -0300

    Adicionar arquivos de projeto.

---
+++ AbrirSolidWorks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace AbrirSolidWorks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //add solidworks reference to project
+            SldWorks.SldWorks swApp;
+
+            
+            try//If there is no solidworks instance open, 
+            {
+                //This function takes an open solidworks instance
+                swApp = Marshal.GetActiveObject("SldWorks.Application") as SldWorks.SldWorks;//To use, a solidworks instance must be open.
+
+            }
+            catch (Exception ex)
+            {
+                
+                //we will catch the error and create a new instance
+                if (ex.HResult == -2147221021)
+                {
+                    swApp = Activator.CreateInstance(Type.GetTypeFromProgID("SldWorks.Application")) as SldWorks.SldWorks;//Its opened in background
+                    swApp.Visible = true;//make the instance visible.
+                }
+            }
+        }
+    }
+}
